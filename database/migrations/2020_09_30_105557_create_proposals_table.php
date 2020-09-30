7f05a6e305a0ad5d775f25c2eaca01a29ef971e2
benben4567 <benben4567@gmail.com>
2020-09-30 18:00:53 +0700

    Create proposals migration table

---
+++ database/migrations/2020_09_30_105557_create_proposals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('scheme_id')->constrained();
+            $table->text('judul');
+            $table->string('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::table('proposals', function (Blueprint $table) {
+          $table->dropForeign('proposals_scheme_id_foreign');
+        });
+        Schema::dropIfExists('proposals');
+    }
+}
