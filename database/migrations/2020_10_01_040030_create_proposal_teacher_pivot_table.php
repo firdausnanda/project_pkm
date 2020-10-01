c3b067565db4e4b09d5ab4200d87945b492406eb
benben4567 <benben4567@gmail.com>
2020-10-01 11:08:32 +0700

    Remove dropForeign

--- database/migrations/2020_10_01_040030_create_proposal_teacher_pivot_table.php
+++ database/migrations/2020_10_01_040030_create_proposal_teacher_pivot_table.php
@@ -27,10 +27,6 @@ class CreateProposalTeacherPivotTable extends Migration
      */
     public function down()
     {   
-        Schema::table('proposal_teacher', function (Blueprint $table) {
-          $table->dropForeign('proposal_teacher_proposal_id');
-          $table->dropForeign('proposal_teacher_teacher_id');
-        });
         Schema::dropIfExists('proposal_teacher');
     }
 }
