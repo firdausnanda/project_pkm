c99111ca1d4aa1e955a0075055d888ba47c4a37d
benben4567 <benben4567@gmail.com>
2020-10-01 11:22:39 +0700

    Add TEacherSeeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
           UserSeeder::class,
           MajorSeeder::class,
           StudentSeeder::class,
+          TeacherSeeder::class
         ]);
     }
 }
