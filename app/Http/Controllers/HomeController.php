1a59185bdfd0f3788eef90dd29a969550a602c14
benben4567 <benben4567@gmail.com>
2020-10-01 09:53:36 +0700

    Change route to landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,11 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('mahasiswa.dashboard');
-    }
-
-    public function adminHome()
-    {
-        return view('home');
+        return view('welcome');
     }
 }
