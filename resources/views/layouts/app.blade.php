efcadff581bb22ff0f3b5d29855b9e540da34551
benben4567 <benben4567@gmail.com>
2020-10-14 11:42:26 +0700

    add asset helper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,16 +6,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="PKM ITSK Soepraoen">
 <meta name="author" content="Firdaus Nanda">
-<link rel="icon" type="image/png" sizes="16x16" href="plugins/images/itsk-logo.png">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('plugins/images/itsk-logo.png')}}">
 <title>PKM Soepraoen</title>
 <!-- Bootstrap Core CSS -->
 <link href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
 <!-- animation CSS -->
-<link href="css/animate.css" rel="stylesheet">
+<link href="{{ asset('css/animate.css')}}" rel="stylesheet">
 <!-- Custom CSS -->
-<link href="css/style.css" rel="stylesheet">
+<link href="{{ asset('css/style.css')}}" rel="stylesheet">
 <!-- color CSS -->
-<link href="css/colors/default.css" id="theme"  rel="stylesheet">
+<link href="{{ asset('css/colors/default.css')}}" id="theme"  rel="stylesheet">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
@@ -34,7 +34,7 @@
       <div class="lg-info-panel">
               <div class="inner-panel">
                   <div class="lg-content">
-                      <img src="plugins/images/ITSK2.png" style="width:150px;" alt="logo-itsk" class="mx-auto d-block">
+                      <img src="{{ asset('plugins/images/ITSK2.png')}}" style="width:150px;" alt="logo-itsk" class="mx-auto d-block">
                       <h1 class="text-white">SISTEM INFORMASI PKM</h1>
                       <h3 class="text-white">Institut Teknologi Sains dan Kesehatan <br> RS dr. Soepraoen</h4>
                       {{-- <p class="text-muted">with this admin you can get 2000+ pages, 500+ ui component, 2000+ icons, different demos and many more... </p> --}}
@@ -42,24 +42,24 @@
               </div>
       </div>
 
-      @yield('konten')
+      @yield('content')
 
 </section>
 
 <!-- jQuery -->
-<script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{{ asset('plugins/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap Core JavaScript -->
-<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{ asset('bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <!-- Menu Plugin JavaScript -->
-<script src="plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<script src="{{ asset('plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js')}}"></script>
 
 <!--slimscroll JavaScript -->
-<script src="js/jquery.slimscroll.js"></script>
+<script src="{{ asset('js/jquery.slimscroll.js')}}"></script>
 <!--Wave Effects -->
-<script src="js/waves.js"></script>
+<script src="{{ asset('js/waves.js')}}"></script>
 <!-- Custom Theme JavaScript -->
-<script src="js/custom.min.js"></script>
+<script src="{{ asset('js/custom.min.js')}}"></script>
 <!--Style Switcher -->
-<script src="plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+<script src="{{ asset('plugins/bower_components/styleswitcher/jQuery.style.switcher.js')}}"></script>
 </body>
 </html>
