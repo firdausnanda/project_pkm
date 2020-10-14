23091e4729f00b5beb9ba444de9e4c4ad9aaaa98
benben4567 <benben4567@gmail.com>
2020-10-14 13:00:14 +0700

    edit Verify view

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,26 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-            <div class="card-body">
-                @if (session('resent'))
-                    <div class="alert alert-success" role="alert">
-                        {{ __('A fresh verification link has been sent to your email address.') }}
-                    </div>
-                @endif
-
-                {{ __('Before proceeding, please check your email for a verification link.') }}
-                {{ __('If you did not receive the email') }},
-                <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                    @csrf
-                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                </form>
-            </div>
-        </div>
-    </div>
+<div class="new-login-box">
+  <div class="white-box">
+    <h3 class="box-title">{{ __('Verify Your Email Address') }}</h3>
+      @if (session('resent'))
+          <div class="alert alert-success" role="alert">
+              {{ __('A fresh verification link has been sent to your email address.') }}
+          </div>
+      @endif
+      <p>
+        {{ __('Before proceeding, please check your email for a verification link.') }}
+        {{ __('Click button below, if you did not receive the email') }},
+      </p>
+      <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+          @csrf
+          <button type="submit" class="btn btn-primary text-uppercase waves-effect waves-light">{{ __('click here to request another') }}</button>.
+      </form>
+  </div>
 </div>
 @endsection
