d857c77c7406b41ded730aa385b6c9ff5374bc3c
benben4567 <benben4567@gmail.com>
2020-10-15 13:28:55 +0700

    add Student relationship

--- app/Major.php
+++ app/Major.php
@@ -8,6 +8,11 @@ class Major extends Model
 {
     protected $fillable = ['degree', 'name'];
 
+    public function students()
+    {
+      return $this->hasMany('App\Student');
+    }
+
     public function getFullNameAttribute()
     {
       return "{$this->degree} {$this->name}";
