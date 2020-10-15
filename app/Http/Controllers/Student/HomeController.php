b75897ed4d163639dbbaf007b7ef673f8fa7eb86
benben4567 <benben4567@gmail.com>
2020-10-15 17:15:21 +0700

    add methods

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
+use App\Major;
+use App\Student;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
@@ -19,6 +25,40 @@ class HomeController extends Controller
 
     public function profile()
     {
-      return view('mahasiswa.data_diri');
+      $majors = Major::all();
+      $student = User::with('student')->find(Auth::user()->id)->student;
+      if ($student) {
+        return view('mahasiswa.data_diri', compact('majors','student'));
+      }
+        return redirect('student/profile/create');
+    }
+
+    public function create()
+    {
+      $majors = Major::all();
+      $student = User::with('student')->find(Auth::user()->id)->student;
+      if ($student) {
+        return redirect('student/profile');
+      }
+      return view('mahasiswa.data_diri_create', compact('majors','student'));
+    }
+
+    public function store(ProfileRequest $request)
+    {
+      dd($request->all());
+    }
+    
+    public function edit()
+    {
+      $majors = Major::all();
+      $student = User::with('student')->find(Auth::user()->id)->student;
+      if ($student) {
+        return view('mahasiswa.data_diri_edit', compact('majors','student'));
+      }
+    }
+
+    public function update(ProfileRequest $req)
+    {
+      dd($req->all());
     }
 }
