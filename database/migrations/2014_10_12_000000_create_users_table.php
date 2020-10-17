a8ca7d261fbfd0a257e296e8a341c5b4afa4374a
benben4567 <benben4567@gmail.com>
2020-10-17 20:48:08 +0700

    add Name column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {   
             $table->id();
+            $table->string('name', 100)->nullable()->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('role',  ['teacher', 'student', 'admin'])->default('student');
