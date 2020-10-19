624d3eb236e3ec62684a24e7868065633f0b190b
benben4567 <benben4567@gmail.com>
2020-10-20 00:27:20 +0700

    add index method

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+      $users = User::whereRole('student')->get();
+      return $users;
+    }
 }
