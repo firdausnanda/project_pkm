892aaa69cfc39c0cd954d884f8f78599fff9cdc1
benben4567 <benben4567@gmail.com>
2020-10-19 23:22:32 +0700

    edit

--- resources/views/admin/akun_teacher_edit.blade.php
+++ resources/views/admin/akun_teacher_edit.blade.php
@@ -11,55 +11,21 @@
 		<!-- <a href="#" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Buy Admin Now</a> -->
 		<ol class="breadcrumb">
 			<li><a href="/admin">Dashboard</a></li>
-			<li class="active">Data Akun Dosen</li>
+			<li><a href="/admin/users/teacher">Data Akun Dosen</a></li>
+			<li class="active">Edit</li>
 		</ol>
 	</div>
 	<!-- /.col-lg-12 -->
 </div>
 
-<!-- Modal Tambah -->
-<div id="tambah-user-teacher" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×
-				</button>
-				<h4 class="modal-title">Input Data User</h4> 
-			</div>
-			<form action="/admin/users/store" method="post" autocomplete="off">
-			<div class="modal-body">
-          @csrf
-          <div class="form-group">
-            <label for="">Nama</label>
-            <input type="text" class="form-control" name="nama" required>
-          </div>
-					<div class="form-group">
-						<label for="" class="control-label">Email</label>
-						<input type="email" class="form-control" id="email" name="email" required=""> 
-					</div>
-					<div class="form-group">
-						<label for="" class="control-label">Password</label>
-						<input type="password" class="form-control" id="password" name="password" required="">
-					</div>
-					<div class="form-group">
-						<label class="control-label">Role</label>           
-						<select class="form-control" name="role">
-							<option>Teacher</option>
-						</select>
-					</div>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close
-				</button>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-			</div>
-			</form>
-		</div>
-	</div>
-</div>
-
 <!-- Container Manage User -->
 <div class="row">
+  {{-- Alert --}}
+  @if(session('success'))
+    <div class="alert alert-success">
+        <strong>Sukses, </strong> {{ session('success') }}
+    </div>
+  @endif
   {{-- Akun --}}
 	<div class="col-md-12 col-lg-6">
 		<div class="panel">
@@ -102,73 +68,97 @@
         Data Profil
       </div>
       <div class="panel-body m-b-0 p-t-0 p-b-0">
-        <form action="" method="post" id="edit-profil-teacher">
+        <form action="{{ route('admin.teacher.update')}}" method="post" id="edit-profil-teacher">
           @csrf
           @method('put')
-          <input type="text" name="id" hidden>
-          <div class="form-group">
+          <input type="text" name="id" value="{{ $user->id }}" hidden>
+          <div class="form-group @error('nama') has-error @enderror">
             <label for="">Nama</label>
             <input type="text" class="form-control" name="nama" value="{{ old('nama', $user->teacher->nama) }}" required>
+            @error('nama')
+                <span class="help-block">{{ $message }}</span>
+            @enderror
           </div>
           <div class="row">
             <div class="col-lg-6">
-              <div class="form-group">
+              <div class="form-group @error('nidn') has-error @enderror">
                 <label for="">NIDN/NIDK</label>
                 <input type="text" class="form-control" name="nidn" value="{{ old('nidn', $user->teacher->nidn) }}" required>
+                @error('nidn')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
             <div class="col-lg-6">
-              <div class="form-group">
+              <div class="form-group @error('major_id') has-error @enderror">
                 <label for="">Program Studi</label>
                 <select class="form-control" name="major_id">
                   @foreach($majors as $major)
                     <option value="{{ $major->id }}" {{ (old('major_id', $user->teacher->major_id) == $major->id) ? "selected" : '' }}>{{ $major->fullname }}</option>
                   @endforeach
                 </select>
+                @error('major_id')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col-lg-4">
-              <div class="form-group">
+              <div class="form-group @error('jk') has-error @enderror">
                 <label for="">Jenis Kelamin</label>
                 <select class="form-control" name="jk">
                   <option value="laki-laki" {{ (old('jk', $user->teacher->jk) == 'laki-laki') ? 'selected' : '' }}>Laki-laki</option>
                   <option value="perempuan" {{ (old('jk', $user->teacher->jk) == 'perempuan') ? 'selected' : '' }}>Perempuan</option>
                 </select>
+                @error('jk')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
             <div class="col-lg-4">
-              <div class="form-group">
+              <div class="form-group @error('tempat_lahir') has-error @enderror">
                 <label for="">Tempat Lahir</label>
-                <input type="text" class="form-control" name="tempat_lahir">
+                <input type="text" class="form-control" name="tempat_lahir" value="{{ old('tempat_lahir', $user->teacher->tempat_lahir) }}">
+                @error('tempat_lahir')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
             <div class="col-lg-4">
-              <div class="form-group">
+              <div class="form-group @error('tgl_lahir') has-error @enderror">
                 <label for="">Tanggal Lahir</label>
                 <input type="text" class="form-control datepicker" name="tgl_lahir" value="{{ old('tgl_lahir', $user->teacher->tgl_lahir) }}">
+                @error('tgl_lahir')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col-lg-6">
-              <div class="form-group">
+              <div class="form-group @error('email') has-error @enderror">
                 <label for="">Email</label>
                 <input type="email"class="form-control" name="email" value="{{ old('email', $user->email) }}">
+                @error('email')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
             <div class="col-lg-6">
-              <div class="form-group">
+              <div class="form-group @error('no_hp') has-error @enderror">
                 <label for="">No. HP</label>
                 <input type="text"class="form-control" name="no_hp" value="{{ old('nidn', $user->teacher->no_hp) }}">
+                @error('no_hp')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
               </div>
             </div>
           </div>
         </form>
       </div>
       <div class="panel-footer">
-        <button type="button" onclick="event.prevenDefault(); document.getElementById('edit-profil-teacher').submit();" class="btn btn-primary">Simpan</button>
+        <button type="button" onclick="event.preventDefault(); document.getElementById('edit-profil-teacher').submit();" class="btn btn-primary">Simpan</button>
       </div>
     </div>
   </div>
